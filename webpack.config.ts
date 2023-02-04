e47a41ce211961b9d12990d5d0c16ea9bda93f17
fifanncher <fifanncher@gmail.com>
2023-02-04 12:52:11 +0500

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import { BuildPath, BuildEnv } from './config/build/types/config';
+import path from 'path';
+import webpack from 'webpack';
+import { BuildWebpackConfig } from './config/build/buildWebpackConfig';
+
+export default (env: BuildEnv) => {
+    const paths: BuildPath = {
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html')
+    }
+    
+    const mode = env.mode || 'development';
+    const PORT = env.port || 3000;
+
+    const isDev = mode === 'development';
+    
+    const config: webpack.Configuration = BuildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port: PORT
+    });
+
+    return config;
+};
