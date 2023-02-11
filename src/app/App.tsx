492441537531288ee05564bf64cdc05a2398ecc4
fifanncher <fifanncher@gmail.com>
2023-02-11 20:35:15 +0500

    done Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,17 @@
-
-import { Suspense } from 'react';
-import { Link } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
             <button onClick={toggleTheme}>TOGGLE THEME</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>ЗагрузОЧКА</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
         </div>
     );
 };
---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+import { Link, LinkProps } from "react-router-dom";
+import { classNames as cn } from "shared/lib/classNames/classNames";
+import s from "./AppLink.module.scss";
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary'
+}
+
+export interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme
+}
+
+export const AppLink: FC<AppLinkProps> = (props: AppLinkProps) => {
+  const { to, className, children, theme = AppLinkTheme.PRIMARY, ...otherProps } = props;
+
+  return (
+    <Link
+        to={to}
+        className={cn(s.AppLink, {}, [className, s[theme]])}
+        {...otherProps}
+    >
+        {children}
+    </Link>
+  );
+};
