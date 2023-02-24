cc60da8e54cadde06e0b93dfa6e2809e92745cad
fifanncher <fifanncher@gmail.com>
2023-02-24 10:59:51 +0500

    add locales

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,5 @@
 import { classNames as cn } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "shared/ui/Widgets";
 import s from "./Navbar.module.scss";
 
 export interface NavbarProps {
@@ -10,7 +9,6 @@ export interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={cn(s.Navbar, {}, [className])}>
-        <ThemeSwitcher />
         <div className={s.links}>
             <AppLink
                 to={'/'}
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { classNames as cn } from "shared/lib/classNames/classNames";
+import { LangSwitcher } from "widgets/LangSwitcher";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+import s from "./Sidebar.module.scss";
+
+export interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar = ({className}: SidebarProps) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed(prev => !prev);
+  };
+
+  return (
+    <div
+      className={cn(s.Sidebar, {[s.collapsed]: collapsed}, [className])}
+    >
+      <button onClick={onToggle}>TOGGLE</button>
+      <div className={s.switchers}>
+        <ThemeSwitcher />
+        <LangSwitcher className={s.lang} />
+      </div>
+    </div>
+  );
+};
